bd9804b4b71a69eef6ef4f78ad9565b1ef84ff2a
Sergiy Sobchuk <SergiySobchuk@gmail.com>
2019-07-05 02:19:31 +0300

    task2 done

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const A = +prompt('Length sides A', 2);
+const B = +prompt('Length sides B', 2);
+const C = +prompt('Length sides C', 2);
+
+if( A + B > C && A + C > B && B + C > A && A > 0 && B > 0 && C > 0){
+    if (A === B && B === C){
+        console.log('Eequivalent triangl');
+    }else if(A===Math.sqrt(Math.pow(B, 2) + Math.pow(C, 2)) || 
+            B===Math.sqrt(Math.pow(A, 2) + Math.pow(C, 2)) ||
+            C===Math.sqrt(Math.pow(B, 2) + Math.pow(A, 2))){
+            console.log('Isosceles triangle');
+    }else{
+        console.log('Normal triangle');
+    }
+}else {
+    console.log('Triangle doesn’t exist');
+}
