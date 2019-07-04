2d54b81c8934186395fb74bcea814ecabd0b951f
Sergiy Sobchuk <SergiySobchuk@gmail.com>
2019-07-05 00:30:56 +0300

    task1 done

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,15 @@
-// Your code goes here
+const a1 = prompt('Enter x-coordinate point A', 0);
+const a2 = prompt('Enter y-coordinate point A', 0);
+const b1 = prompt('Enter x-coordinate point B', 4);
+const b2 = prompt('Enter y-coordinate point B', 4);
+const c1 = prompt('Enter x-coordinate point C', 2);
+const c2 = prompt('Enter y-coordinate point C', 2);
+
+const checkLineCenterX = (a1+b1)/2; 
+const checkLineCenterY = (a2+b2)/2;
+
+if(checkLineCenterX === +c1 && checkLineCenterY === +c2){
+    console.log('true');
+} else {
+    console.log('false');
+}
