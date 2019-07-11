0417af2aa9fc2d1910441b10dfcbc7640fbd0537
SergiySobchuk <Sergiy.Sobchuk@gmail.com>
2019-07-11 07:30:52 +0300

    edit task1-2, add tmp html

--- FL11_HW8/homework/js/isBigger.js
+++ FL11_HW8/homework/js/isBigger.js
@@ -1,6 +1,6 @@
 function isBigger(first, second){
 return first > second;
 }
-console.log(isBigger(5,1));
+console.log('isBigger(5,-1)// =>',isBigger(5,-1));
 
     
--- FL11_HW8/homework/js/isSmaller.js
+++ FL11_HW8/homework/js/isSmaller.js
@@ -1,4 +1,4 @@
 function isSmaller(first, second){
     return first < second;
     }
- console.log(isSmaller(5,1));
+ console.log('isSmaller(5,-1)// =>',isSmaller(5,1));
