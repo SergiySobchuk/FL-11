26bb3375d51764c36e1c50200d92619ff1868624
Sergiy Sobchuk <SergiySobchuk@gmail.com>
2019-07-12 19:24:42 +0300

    task 3 done

---
+++ FL11_HW8/homework/js/getMin.js
@@ -0,0 +1,10 @@
+function getMin(){
+    let min = 0;
+    for (let i = 0;  i < (arguments.length); i++) {
+        if (arguments[i] < min){
+            min = arguments[i];
+        }
+    }
+    return min;
+} 
+console.log(getMin(3, 0, -3, 5, 6, 7, 3, -5, 6, 3, 2, 100, -8));
