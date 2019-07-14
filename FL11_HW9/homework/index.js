267774d3044671bd1f47e48ab3ffa5cc5f3378c3
Sergiy Sobchuk <SergiySobchuk@gmail.com>
2019-07-14 11:35:34 +0300

    task-1 done

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+console.log('*************task1**************');
+
+function getNumbers(inStr){
+    let inArr = inStr.split('');
+    let outArr = [];
+    inArr.forEach(element => {
+        if (!isNaN(parseInt(element))){
+            outArr.push(parseInt(element));
+        }
+    });
+    return outArr;
+}
+console.log(getNumbers('1e2y3wd-sd5ds7fadsf9@ds'));
+
+console.log('*************task2**************');
+
