6c5b739e019e7bc79eb8e0d2548a3c6d8101c024
Sergiy Sobchuk <SergiySobchuk@gmail.com>
2019-07-14 12:02:55 +0300

    task-2 done

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -14,3 +14,21 @@ console.log(getNumbers('1e2y3wd-sd5ds7fadsf9@ds'));
 
 console.log('*************task2**************');
 
+function findTypes(){
+    let arrayCountType = {object: 0, number: 0, boolean: 0, string: 0};
+    for (let i = 0; i < arguments.length; i++) {
+        if(typeof arguments[i] === 'number'){
+            arrayCountType.number =+ 1;
+        }else if(typeof arguments[i] === 'string'){
+            arrayCountType.string =+ 1;
+        }else if (typeof arguments[i] === 'boolean'){
+            arrayCountType.boolean =+ 1;
+        }else if (typeof arguments[i] === 'object'){
+            arrayCountType.object =+ 1;
+        }
+    }
+    return arrayCountType;
+}
+console.log(findTypes(5, 'hello', false, null));
+
+console.log('*************task3**************');
