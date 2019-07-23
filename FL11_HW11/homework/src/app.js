9a1ac668ff2f65cb286e43cac289d25c00fcfe27
SergiySobchuk <Sergiy.Sobchuk@gmail.com>
2019-07-23 23:56:18 +0300

    create TODO task done

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,3 +1,21 @@
-let rootNode = document.getElementById("root");
-
-// Your code goes here
+let rootNode = document.getElementById('root');
+class Task{
+    constructor(){
+        this.plusTask();
+    }
+    plusTask(){
+        this.plus = document.getElementById('add-block-plus');
+        this.plus.addEventListener('click', this.render.bind(this));
+    }
+    render(){
+        this.addLine = document.getElementById('add-block-text');
+        this.cheBox = document.createElement('input');
+        this.cheBox.setAttribute('type', 'checkbox');
+        this.task = document.createElement('input');
+        this.task.classList.add('task');
+        this.task.value = this.addLine.value;
+        rootNode.append(this.cheBox);
+        rootNode.append(this.task);
+    }
+}
+new Task();
